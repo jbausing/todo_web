475d9fe2aa315c1c2f288c9368b0a5051a1e8039
nine063 <shammahshem.raquinio@oodc.com.ph>
2025-10-02 13:44:11 +0800

    add ui

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Check, X } from "lucide-react";
 
 type Todo = {
   id: number;
@@ -11,12 +22,12 @@ type Todo = {
 
 function Home() {
   const [todo, setTodo] = useState<Todo[]>([
-    {
-      id: 1,
-      desc: "sample task",
-      date: "2025-09-25",
-      status: 1,
-    },
+    // {
+    //   id: 1,
+    //   desc: "sample task",
+    //   date: "2025-09-25",
+    //   status: 1,
+    // },
   ]);
 
   const [input, setInput] = useState("");
@@ -37,17 +48,72 @@ function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center p-4 ">
-      <div className="flex w-3/5 gap-2">
-        <Input value={input} onChange={(e) => setInput(e.target.value)} />
-        <Button onClick={addTask}>Add</Button>
+    <div className="flex flex-col items-center p-20 ">
+      <nav className="absolute top-6 left-8 flex items-center gap-8 text-sm">
+        <Sheet>
+          <SheetTrigger className="font-[Poppins]">History</SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid flex-1 auto-rows-min gap-6 px-4">
+              <div className="grid gap-3">
+                <label htmlFor="sheet-demo-name">Name</label>
+                <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <label htmlFor="sheet-demo-username">Username</label>
+                <Input id="sheet-demo-username" defaultValue="@peduarte" />
+              </div>
+            </div>
+            <SheetFooter>
+              <Button type="submit">Save changes</Button>
+              <SheetClose asChild>
+                <Button variant="outline">Close</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+        {/* <span className="font-[Poppins]">History</span> */}
+        <span className="font-[Poppins]">Dark mode</span>
+      </nav>
+
+      <div className="w-full max-w-3xl">
+        {/* headline */}
+        <h1 className="font-[Poppins] font-extrabold text-[100px] leading-none tracking-tight text-black text-center">
+          Get it done.
+        </h1>
       </div>
 
-      <div className="flex flex-col w-3/5 pt-4">
-        <div>Your Task: </div>
+      <div className="flex w-3/5">
+        <Input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a task"
+          className="bg-black text-white h-10 px-4 outline-none focus-visible:ring-0 focus:ring-0 ring-0 shadow-none !rounded-none"
+        />
+        <Button
+          onClick={addTask}
+          className="font-[Poppins] text-xs font-semibold bg-[#EFEFEF] h-10 px-4 text-black hover:!bg-[#EFEFEF] hover:!text-black !rounded-none"
+        >
+          I'm on it
+        </Button>
+      </div>
+
+      <div className="flex flex-col w-3/5 pt-4 space-y-2">
+        <div className="font-[Poppins] text-center">Your Task: </div>
         {todo.map((item) => (
-          <div key={item.id} className="pl-4 flex justify-between">
+          <div
+            key={item.id}
+            className="mx-auto flex items-center justify-between border border-black w-[468px] h-10 px-3"
+          >
             <p>{item.desc}</p> <p>{item.date}</p>
+            <X />
+            <Check />
           </div>
         ))}
       </div>
